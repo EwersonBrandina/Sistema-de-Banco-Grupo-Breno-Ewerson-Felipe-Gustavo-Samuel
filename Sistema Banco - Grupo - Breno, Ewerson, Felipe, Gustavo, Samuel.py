3b55783e6bdabb2148ef7734c5664b04ff04d527
Ewerson.Brandina <ewerson.brandina@hotmail.com.br>
2022-06-13 11:17:20 -0300

    Sistema Banco - 1º Commit - Modelo Padrão e Uma Janela

--- Sistema Banco - Grupo - Breno, Ewerson, Felipe, Gustavo, Samuel.py
+++ Sistema Banco - Grupo - Breno, Ewerson, Felipe, Gustavo, Samuel.py
@@ -1,2 +1,57 @@
+#from tkinter import *
+
+#config tela
+
+
+#root = Tk()
+#root.geometry('400x300+720+350') # declara o tamanho da jenela (400x300) tamanho inicial/ +100 +100 lugar onde ela vai abrir 
+#root.config(background='#fff') #background color
+
+#-------------------------- Functions --------------------------#
+
+#Aqui ficaram as funções do projeto
+
+#-------------------------- Frame 0 / Widgets --------------------------#
+
+#frX = LabelFrame(root, background='#fff', text='text', fg="gray", font='Georgia 15')
+# X é o numero a ser substiuido pelo numero da sua flame
+
+#lb0_frX = Label(frX, text="text", font='Georgia 15')
+#Label padronizado
+#bt0_frX = Button(frX, text="text", font='Georgia 15')
+#Botão padronizado
+#in0_frX = Entry(frX, font='Georgia 15')
+#Entrada de dados (input) padronizado
+
+#-------------------------- Frame 1 / Apresentar --------------------------#
+
+#frX.grid()
+#grid / para mostrar o flame
+#lb0_frX.grid()
+#grid / para mostrar o Label
+#in0_frX.grid()
+#grid / para mostrar a entrada de dados
+
+#-------------------------- Mostrar tela --------------------------#
+
+#root.mainloop()
+
+#INÍCIO DO CÓDIGO
+#Frame 0 - Gustavo
+
+#Frame 1 - Samuel
+
+#Frame 2 - Breno
+
+#Frame 3 - Felipe
+
+#Frame 4 - Ewerson
 from tkinter import *
 
+root = Tk()
+root.title('Banco ___')
+fr4 = LabelFrame(root, padx=10, pady=10, bg='#49A', text='Usuário', font='Arial 25', borderwidth=1, relief="sunken")
+
+lb0_fr4
+
+root.mainloop()
