d486fbd7459be8b709b8cf7cd029f2f97c23e0fe
Ewerson.Brandina <ewerson.brandina@hotmail.com.br>
2022-06-20 10:50:06 -0300

    Atividade Incompleta - 20/06/2022 - 10:50

--- classe_cliente.py
+++ classe_cliente.py
@@ -1,9 +1,17 @@
 class Cliente:
 
-    def __init__(self, nome: str, cpf: str, dataNasc: str):
+    def __init__(self, nome: str, cpf: str, dataNasc: str, telefone: str, uf: str, logradouro: str, numero: str, bairro: str, cidade: str, email: str, senha: str):
         self.nome = nome
         self.cpf = cpf
         self.dataNasc = dataNasc
+        self.telefone = telefone
+        self.uf = uf
+        self.logradouro = logradouro
+        self.numero = numero
+        self.bairro = bairro
+        self.cidade = cidade
+        self.email = email
+        self.senha = senha
 
     def imprime_dados(self):
         print(f'Nome: {self.nome}\ncpf: {self.cpf}\n'
