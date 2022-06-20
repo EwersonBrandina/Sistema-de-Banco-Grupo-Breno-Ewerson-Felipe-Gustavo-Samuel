6121352acc2319780d79125dcedcf0924f66eb8d
Samuel <samuelcauabarbosa114@gmail.com>
2022-06-20 09:23:46 -0300

    atualização

--- classe_cliente.py
+++ classe_cliente.py
@@ -1,10 +1,18 @@
 class Cliente:
 
-    def __init__(self, nome: str, cpf: str, dataNasc: str):
+    def __init__(self, nome: str, cpf: str, dataNasc: str, telefone: str, uf: str,logradouro:str,numero: str, bairro:str, cidade:str, email:str, senha:str):
         self.nome = nome
         self.cpf = cpf
         self.dataNasc = dataNasc
+        self.telefone = telefone
+        self.uf = uf
+        self.logradouro = logradouro
+        self.numero = numero
+        self.bairro = bairro
+        self.cidade = cidade
+        self.email = email
+        self.senha = senha
 
     def imprime_dados(self):
         print(f'Nome: {self.nome}\ncpf: {self.cpf}\n'
-              f'Data de Nasc: {self.dataNasc}')
+              f'Data de Nasc: {self.dataNasc}')        
