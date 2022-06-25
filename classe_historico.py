640b110fd17fc7a00b5357cb00fcb7b7884fe65a
EwersonBrandina <106076391+EwersonBrandina@users.noreply.github.com>
2022-06-25 16:01:36 -0300

    Update classe_historico.py

--- classe_historico.py
+++ classe_historico.py
@@ -1,10 +1,10 @@
 class Historico:
 
-    def __init__(self, msg='Transações: '):
-        self.transacoes = []
+    def __init__(self, msg= 'Transferência: ', transacoes = []):
+        self.transacoes = transacoes
         self.msg = msg
 
     def transacoes_bancarias(self):
-        self.msg = 'Transações: '
+        self.msg = 'Transações:'
         for i in self.transacoes:
             self.msg += '\n - ' + i
